4d9628f9215e19b7f8f8bb90299be5361783d4e7
WRAAATH <gitlab@wraaath.xyz>
2022-11-24 12:49:22 +0100

    Fixed Pythagoras & added al

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 use std::io;
 use std::io::Write;
+use std::cmp::Ordering;
 
 
-// fuck dig Linus
-fn read_integer(prompt: &str) -> i64 {
-    let input = read_input(prompt);
-    return input.parse::<i64>().expect("Input er ikke et tal.");
+// Integer-input
+fn read_float(prompt: &str) -> f32 {
+    let input = read_string(prompt);
+    return input.parse::<f32>().expect("Input er ikke et tal.");
 }
 
-// Read user input function
-fn read_input(prompt: &str) -> String {
+// String-input
+fn read_string(prompt: &str) -> String {
     // write prompt
     print!("{}: ", prompt);
     io::stdout().flush().unwrap();
 
-    // read user input
+    // Læs brugerinput
     let mut read = String::new();
     io::stdin().read_line(&mut read).expect("Input er ugyldigt.");
     read = read.trim().to_string(); // ???
@@ -28,22 +29,28 @@ fn main() {
     println!("GYM CALCULATOR v2 (jannick æd lort)");
     println!("calc - Basics (+, -, *, /, ^, sqrt)");
     println!("calc py - Pythagoras");
+    println!("calc al - Andengradsligning");
     println!("");
 
     
-    let choice = read_input("Input");
+    let choice = read_string("Input");
     match choice.as_str() {
-        "calc" => basics(),
+
         "calc py" => pythagoras(),
+        "calc al" => andengradsligning(),
         _ => println!("Ugyldigt input"),
     }
     println!("");
 }
 
 // Basics
-fn basics() {
-    println!("+, -, *, /, ^, sqrt");
-}
+//fn basics() {
+//    println!("+, -, *, /, ^, sqrt");
+//    let basics_a = read_float("Input");
+
+    //Calculate basics_a
+    
+//}
 
 
 
@@ -51,28 +58,46 @@ fn basics() {
 // Pythagoras
 fn pythagoras() {
     println!("Indtast a, b & c-værdierne, men hold den ukendte værdi til 0");
-    let py_a = read_integer("a");
-    let py_b = read_integer("b");
-    let py_c = read_integer("c");
+    let py_a = read_float("a");
+    let py_b = read_float("b");
+    let py_c = read_float("c");
 
-    //Calculate py_ans
-    let py_ans = ((py_a.pow(2) + py_b.pow(2) + py_c.pow(2)) as f32).sqrt();
+    //Udregn py_ans
+    let py_ans = (py_a.powf(2.0) + py_b.powf(2.0) + py_c.powf(2.0)).sqrt();
 
-    //Print answer depending on what value is 0
-    if py_a == 0 {
+    //Print svar afhængigt af 0
+    if py_a == 0.0 {
         println!("");
         println!("b² + c² = a²");
         println!("{}² + {}² = {}²", py_b, py_c, py_ans);
     }
-    if py_b == 0 {
+    if py_b == 0.0 {
         println!("");
         println!("a² + c² = b²");
         println!("{}² + {}² = {}²", py_a, py_c, py_ans);
     }
-    if py_c == 0 {
+    if py_c == 0.0 {
         println!("");
         println!("a² + b² = c²");
         println!("{}² + {}² = {}²", py_a, py_b, py_ans);
     }
+}
 
+// Andengradsligning
+fn andengradsligning() {
+    println!("Indtast dine a, b & c-værdier");
+    let al_a = read_float("a");
+    let al_b = read_float("b");
+    let al_c = read_float("c");
+
+    // Udregn andengradsligning
+    let al_d = (al_b.powf(2.0) - 4.0 * al_a * al_c).sqrt();
+    println!("{}", al_d);
+
+    // Match al_d
+    match al_d.partial_cmp(&0.0).unwrap() {
+        Ordering::Less => println!("Ingen løsning"),
+        Ordering::Equal => println!("gaide2"),
+        Ordering::Greater => println!("gaide3"),
+    }
 }
